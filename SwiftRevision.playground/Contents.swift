2682a9a9a88417138323c186541c0ef49ff9396e
medahlia <matvienko.dash.0k@gmail.com>
2026-03-05 18:57:37 +0200

    Add checkpoint 1

--- SwiftRevision.playground/Contents.swift
+++ SwiftRevision.playground/Contents.swift
@@ -1,3 +1,8 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// MARK: converting temperatures from Celsius to Fahrenheit
+let celsius: Double = 18
+var fahrenheit: Double
+fahrenheit = celsius * 9 / 5 + 32
+print("from Celsius: \(celsius) \nto Fahrenheit: \(fahrenheit)")
+
