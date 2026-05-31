bdfd28df220a4de1d043bff5bff77b26e6c78216
medahlia <matvienko.dash.0k@gmail.com>
2026-05-31 18:55:00 +0300

    init project

---
+++ Executive Timer/Executive Timer/Executive_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Executive_TimerApp.swift
+//  Executive Timer
+//
+//  Created by Home on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Executive_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
