27f2b901ca0d0d665f55ee312f00ff97fe45e108
medahlia <matvienko.dash.0k@gmail.com>
2026-05-31 13:47:08 +0300

    init project

---
+++ Todo List/Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Home on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
